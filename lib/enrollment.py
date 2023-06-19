492165ae26e3fe95caea2833180b406419516485
matthew rendely <matthew.rendely@gmail.com>
2023-06-19 11:05:28 -0700

    Done

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -3,6 +3,7 @@ class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -14,6 +15,20 @@ class Student:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+
+    def course_count(self):
+        return len(self._enrollments)
+
+    def add_grade(self, letter_grade):
+        self._grades[self._enrollments[0]] = letter_grade
+
+    def aggregate_average_grade(self):
+        # lets assume the grades are stored in a protected attribute called _grades. 
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+
+        return average_grade    
 
 class Course:
     def __init__(self, title):
@@ -32,13 +47,33 @@ class Course:
 
 
 class Enrollment:
+    all  = []
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
             self.course = course
             self._enrollment_date = datetime.now()
+            Enrollment.all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
+
+student1 = Student('Hailey')
+course1 = Course('Math')
+course1 = Course('History')
+print(student1.course_count())
+student1.enroll(course1)
+print(student1.course_count())
+student1.add_grade(3.0)
+print(Enrollment.aggregate_enrollments_per_day())
+print(student1.aggregate_average_grade())
